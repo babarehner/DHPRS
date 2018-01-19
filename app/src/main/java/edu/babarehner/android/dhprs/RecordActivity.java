4826c95d7ab21da1c99a552eb6a0402ba7e3fd52
Mike Rehner <babarehner@gmail.com>
2018-01-19 10:36:23 -0500

    Changed minimum API level from 15 to 16 Commented out RecordProvider to get app to run.

---
+++ app/src/main/java/edu/babarehner/android/dhprs/RecordActivity.java
@@ -0,0 +1,16 @@
+package edu.babarehner.android.dhprs;
+
+import android.app.LoaderManager;
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mike on 1/18/18.
+ */
+
+public class RecordActivity extends AppCompatActivity {
+
+    //TODO implements LoaderManager.LoaderCallbacks<Cursor>
+
+
+}
--- app/src/main/java/edu/babarehner/android/dhprs/data/RecordProvider.java
+++ app/src/main/java/edu/babarehner/android/dhprs/data/RecordProvider.java
@@ -6,13 +6,16 @@ import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
+import static edu.babarehner.android.dhprs.data.RecordContract.RecordEntry.TDHPRS;
+import static edu.babarehner.android.dhprs.data.RecordDbHelper.DB_NAME;
+
 
 /**
  * Created by mike on 1/18/18.
  */
 
 
-public class RecordProvider extends ContentProvider {
+public class RecordProvider {
 
     public static final String LOG_TAG = RecordProvider.class.getSimpleName();
 
@@ -23,7 +26,7 @@ public class RecordProvider extends ContentProvider {
         //sUriMatcher.addURI(...);
     }
 
-
+/*
 
     @Override
     public boolean onCreate(){
@@ -33,23 +36,30 @@ public class RecordProvider extends ContentProvider {
 
     public String getType(Uri uri){
         //TODO
+        return "Dummy";
     }
 
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
-                        String sortOrder) {
-
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        int a[] = {0,1};
+        Cursor c = a;
+        return c;
     }
 
     public Uri insert(Uri uri, ContentValues values) {
         //TODO
+        return uri;
     }
 
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
         //TODO
+
+        return 0;
     }
 
     public int delete(Uri uri, String selection, String[] selectionArgs) {
         //TODO
+        return 0;
     }
+    */
 }
 
