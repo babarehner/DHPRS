7562e71496bcb3956513de0edf17a72fbb305738
Mike Rehner <babarehner@gmail.com>
2018-01-20 16:54:53 -0500

    Wire up spinners to show drop down menu

--- app/src/main/java/edu/babarehner/android/dhprs/RecordActivity.java
+++ app/src/main/java/edu/babarehner/android/dhprs/RecordActivity.java
@@ -17,19 +17,21 @@ public class RecordActivity extends AppCompatActivity {
     //TODO implements LoaderManager.LoaderCallbacks<Cursor>
 
     public static final Integer[] RATINGS = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    Context context;
+    public static final CharSequence[] PRACTYPE ={"Other", "Phone App", "Recording"};
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
 
         setTitle("Add Records");
 
+        setUpSpinners();
+
     }
 
+    // TODO wire up spinners to show up data
     // populate the spinner automatically instead of using arrays.xml
-    private void setUpSpinner() {
+    private void setUpSpinners() {
 
         Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
         ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this,
@@ -37,6 +39,34 @@ public class RecordActivity extends AppCompatActivity {
         // Specify the layout to use when the list of choices appear
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(adapter); // apply the adapter to the spinner
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter<Integer> adapter2 = new ArrayAdapter<Integer>(this,
+                android.R.layout.simple_spinner_item, RATINGS);
+        // Specify the layout to use when the list of choices appear
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter); // apply the adapter to the spinner
+
+        Spinner spinner3 = (Spinner) findViewById(R.id.spinner3);
+        ArrayAdapter<Integer> adapter3 = new ArrayAdapter<Integer>(this,
+                android.R.layout.simple_spinner_item, RATINGS);
+        // Specify the layout to use when the list of choices appear
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter); // apply the adapter to the spinner
+
+        Spinner spinner4 = (Spinner) findViewById(R.id.spinner4);
+        ArrayAdapter<Integer> adapter4 = new ArrayAdapter<Integer>(this,
+                android.R.layout.simple_spinner_item, RATINGS);
+        // Specify the layout to use when the list of choices appear
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner4.setAdapter(adapter); // apply the adapter to the spinner
+
+        Spinner spinnerPracType = (Spinner) findViewById(R.id.prac_type);
+        ArrayAdapter<CharSequence> adapter_pracType = ArrayAdapter.createFromResource(this,
+                R.array.spinner_practype, android.R.layout.simple_spinner_item);
+        adapter_pracType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerPracType.setAdapter(adapter_pracType);
+
     }
 
 }
