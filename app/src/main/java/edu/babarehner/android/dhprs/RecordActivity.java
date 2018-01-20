2e8fa51b71421b11865c1e0d5cc010234516c9c8
Mike Rehner <babarehner@gmail.com>
2018-01-19 22:36:14 -0500

    Created activity_record layout page in xml Set up spinners in array.xml and in code

--- app/src/main/java/edu/babarehner/android/dhprs/RecordActivity.java
+++ app/src/main/java/edu/babarehner/android/dhprs/RecordActivity.java
@@ -3,6 +3,8 @@ package edu.babarehner.android.dhprs;
 import android.app.LoaderManager;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by mike on 1/18/18.
@@ -12,5 +14,17 @@ public class RecordActivity extends AppCompatActivity {
 
     //TODO implements LoaderManager.LoaderCallbacks<Cursor>
 
+    public static final Integer[] RATINGS = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    // populate the spinner automatically instead of using arrays.xml
+    private void setUpSpinner() {
+
+        Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this,
+                android.R.layout.simple_spinner_item, RATINGS);
+        // Specify the layout to use when the list of choices appear
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter); // apply the adapter to the spinner
+    }
 
 }
