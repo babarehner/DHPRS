3e415b27555a901e425bbe4a08e4b75bbaa04044
Mike Rehner <babarehner@gmail.com>
2018-01-21 18:59:32 -0500

    Wire up time picker

--- app/src/main/java/edu/babarehner/android/dhprs/RecordActivity.java
+++ app/src/main/java/edu/babarehner/android/dhprs/RecordActivity.java
@@ -3,6 +3,7 @@ package edu.babarehner.android.dhprs;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.LoaderManager;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
@@ -28,9 +30,9 @@ public class RecordActivity extends AppCompatActivity {
 
     public static final Integer[] RATINGS = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     public static final CharSequence[] PRACTYPE ={"Other", "Phone App", "Recording"};
-    private TextView tvDate;
-    private Button pickDate;
-    int mYear, mMonth, mDay;
+    private TextView tvDate, tvTime;
+    private Button pickDate, pickTime;
+    int mYear, mMonth, mDay, mHour, mMinute;
 
     static final int DATE_DIALOG_ID = 0;
     static final int TIME_DIALOG_ID = 1;
@@ -45,6 +47,7 @@ public class RecordActivity extends AppCompatActivity {
         setUpSpinners();
 
         getDate();
+        getTime();
 
 
 
@@ -71,22 +74,15 @@ public class RecordActivity extends AppCompatActivity {
         mDay = c.get(Calendar.DAY_OF_MONTH);
     }
 
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        switch(id) {
-            case DATE_DIALOG_ID:
-                return new DatePickerDialog(this, DateSetListener, mYear, mMonth, mDay);
-        }
-        return null;
-    }
+
 
     //updates the date displayed in TextView
     private void updateDisplay() {
          tvDate.setText(
          new StringBuilder()
-                 .append((mMonth + 1) +"/")
-                 .append(mDay + "/")
-                 .append(mYear + " "));
+                 .append((mMonth + 1)).append("/")
+                 .append(mDay).append("/")
+                 .append(mYear).append(" "));
     }
 
     // the callback received when the user sets the date in the dialog
@@ -102,33 +98,79 @@ public class RecordActivity extends AppCompatActivity {
             };
 
 
+    public void getTime() {
+        // get View elements
+        tvTime = (TextView) findViewById(R.id.tvTime);
+        pickTime = (Button) findViewById(R.id.pick_time);
+        // add a click listener to the button
+        pickTime.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(TIME_DIALOG_ID);
+            }
+        }));
+
+        // get the current time
+        final Calendar t = Calendar.getInstance();
+        mHour = t.get(Calendar.HOUR_OF_DAY);
+        mMinute = t.get(Calendar.MINUTE);
+
+        // display the current time
+        updateTime();
+    }
+
+    private void updateTime() {
+        tvTime.setText(new StringBuilder().append(pad(mHour)).append(":").append(pad(mMinute)));
+    }
+
+    // the callback received when the user sets the time in the dialog
+    private TimePickerDialog.OnTimeSetListener TimeSetListener =
+            new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    mHour = hourOfDay;
+                    mMinute = minute;
+                }
+            };
+
+
+
+    protected Dialog onCreateDialog(int id) {
+        switch(id) {
+            case DATE_DIALOG_ID:
+                return new DatePickerDialog(this, DateSetListener, mYear, mMonth, mDay);
+            case TIME_DIALOG_ID:
+                return new TimePickerDialog(this, TimeSetListener, mHour, mMinute, false);
+        }
+        return null;
+    }
 
     // populate the spinner automatically instead of using arrays.xml
     private void setUpSpinners() {
 
         Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
-        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this,
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, RATINGS);
         // Specify the layout to use when the list of choices appear
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(adapter); // apply the adapter to the spinner
 
         Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
-        ArrayAdapter<Integer> adapter2 = new ArrayAdapter<Integer>(this,
+        ArrayAdapter<Integer> adapter2 = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, RATINGS);
         // Specify the layout to use when the list of choices appear
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(adapter); // apply the adapter to the spinner
 
         Spinner spinner3 = (Spinner) findViewById(R.id.spinner3);
-        ArrayAdapter<Integer> adapter3 = new ArrayAdapter<Integer>(this,
+        ArrayAdapter<Integer> adapter3 = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, RATINGS);
         // Specify the layout to use when the list of choices appear
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner3.setAdapter(adapter); // apply the adapter to the spinner
 
         Spinner spinner4 = (Spinner) findViewById(R.id.spinner4);
-        ArrayAdapter<Integer> adapter4 = new ArrayAdapter<Integer>(this,
+        ArrayAdapter<Integer> adapter4 = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, RATINGS);
         // Specify the layout to use when the list of choices appear
         adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -140,6 +182,16 @@ public class RecordActivity extends AppCompatActivity {
         adapter_pracType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerPracType.setAdapter(adapter_pracType);
 
+    }
+
+    // puts a 0 in front of a single digit time number
+    private static String pad(int c) {
+        if (c >= 10) {
+            return String.valueOf(c);
+        }
+        else {
+            return "0" + String.valueOf(c);
+        }
     }
 
 }
