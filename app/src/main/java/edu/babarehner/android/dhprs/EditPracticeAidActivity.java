59ef27394b268e242bdeff5a8b3821c61541b259
Mike Rehner <babarehner@gmail.com>
2018-09-07 21:09:28 -0400

    Subject Line: Partial Implement EditPracticeAidActivity
    What Happened:
    Implement onLoadFinished
    Implement onLoaderReset
    Implement activity_edit_practice_aid.xml file
    Set titles for EditPracticalAidActivity screens

--- app/src/main/java/edu/babarehner/android/dhprs/EditPracticeAidActivity.java
+++ app/src/main/java/edu/babarehner/android/dhprs/EditPracticeAidActivity.java
@@ -10,6 +10,7 @@ import android.support.annotation.Nullable;
 import android.content.CursorLoader;
 import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 import edu.babarehner.android.dhprs.data.RecordContract;
 
@@ -18,19 +19,24 @@ public class EditPracticeAidActivity extends AppCompatActivity implements Loader
     private Uri mCurrentRecordUri;
     static final int EXISTING_PRACTICE_AID_LOADER = 3;
 
+    private String mPracticeAid;
+    private EditText mPracticeAidEditText;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_edit_practice_aid);
+        setContentView(R.layout.activity_edit_practice_aid);
 
         Intent intent = getIntent();
         mCurrentRecordUri = intent.getData();
 
         if (mCurrentRecordUri == null) {
-            // setTitle(getString(R.string.practice_aid_activity_title_add_record));
+            setTitle(getString(R.string.practice_aid_activity_title_add_record));
         } else {
-            // setTitle(getString(R.string.practice_aid_activity_edit_record));
+            setTitle(getString(R.string.practice_aid_activity_edit_record));
             getLoaderManager().initLoader(EXISTING_PRACTICE_AID_LOADER, null, EditPracticeAidActivity.this);
         }
+
+        mPracticeAidEditText = (EditText) findViewById(R.id.edit_practice_aid);
     }
 
 
@@ -45,13 +51,24 @@ public class EditPracticeAidActivity extends AppCompatActivity implements Loader
 
 
     @Override
-    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor c) {
+        // move to the only row in the cursor
+        if (c.moveToFirst()){
+            int practiceAidColIndex = c.getColumnIndex(RecordContract.RecordEntry.CPRAC_AIDS);
+            // use the index to pull the data out
+            mPracticeAid = c.getString(practiceAidColIndex);
+            // update the text view
+            mPracticeAidEditText.setText(mPracticeAid);
+
+
+        }
 
     }
 
 
     @Override
     public void onLoaderReset(@NonNull Loader<Cursor> loader) {
-
+        // if invalid loader clear data from input field
+        mPracticeAidEditText.setText("");
     }
 }
